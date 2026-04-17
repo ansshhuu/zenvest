38ccf58e1934aba5768ae6e3e2068a65b91dff8f
Anshu Panwar <anshumona2005@gmail.com>
2026-04-17 23:56:14 +0530

    Delete frontend directory

--- frontend/src/utils/toast.js
+++
@@ -1,6 +0,0 @@
-// Exported for use outside React tree if needed
-export function showToast(message, type = "info") {
-  // This is handled by React state in App.jsx
-  // This file exists as a placeholder for non-React contexts
-  console.log(`[Toast][${type}] ${message}`);
-}
